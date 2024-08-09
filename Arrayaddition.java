30868d7fc66608f9a818d016be22a87988b70cc6
MADHANSHARON <madhanag2017@gmail.com>
2024-08-09 20:11:50 +0530

    updated

--- Arrayaddition.java
+++ Arrayaddition.java
@@ -7,25 +7,26 @@ import java.util.*;
     
     public static void main(String[] args) {
         
-        Scanner sa=new Scanner(System.in);
-        System.out.println("enter size");
-        int se=sa.nextInt();
-        int [] a=new int[se];
-        System.out.println("enter array elements");
-        
-        int r=se-1;
-        for (int i=0;i<se;i++){
-             a[i]=sa.nextInt();
-        }
-        int b=a[r]+1;
-        int []newarr=new int[se];
-        for(int j=0;j<se;j++){
-            newarr[j]=a[j];
-        }
-        newarr[se-1]=b;
-        System.out.println("After");
-        for(int h=0;h<se;h++){
-           System.out.println(newarr[h]);
+        try (Scanner sa = new Scanner(System.in)) {
+            System.out.println("enter size");
+            int se=sa.nextInt();
+            int [] a=new int[se];
+            System.out.println("enter array elements");
+            
+            int r=se-1;
+            for (int i=0;i<se;i++){
+                 a[i]=sa.nextInt();
+            }
+            int b=a[r]+1;
+            int []newarr=new int[se];
+            for(int j=0;j<se;j++){
+                newarr[j]=a[j];
+            }
+            newarr[se-1]=b;
+            System.out.println("After");
+            for(int h=0;h<se;h++){
+               System.out.println(newarr[h]);
+            }
         }
         
     }
